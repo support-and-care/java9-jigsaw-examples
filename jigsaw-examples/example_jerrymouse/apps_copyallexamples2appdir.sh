8366e7676d7b703b678f82e34928b7164716fd65
Gerd Aschemann <gerd@aschemann.net>
2025-10-23 11:07:05 +0200

    Fixed 'JerryMouse Application-Server' example
    The example works slightly differently. It relies on some other examples
    and first needs to create/copy those into the local
    "apps" directory to later load and execute them.
    Execute the apps in lexicographical order to make output deterministic.
    Also add new flags to run "apps" sequentially (not parallel) to make
    the output even more deterministic. Nevertheless, write output of each
    application to separate file to make failure analysis easier.
    
    Introduced in the course of support-and-care/maven-support-and-care#137

--- jigsaw-examples/example_jerrymouse/apps_copyallexamples2appdir.sh
+++ jigsaw-examples/example_jerrymouse/apps_copyallexamples2appdir.sh
@@ -1,44 +1,47 @@
 #!/usr/bin/env bash
+
+set -eu -o pipefail
+
 #
 # Copy all apps from our app whitelist (various example will not work, e.g. when we provide additional options in the run script)
 #
 
 # source the list of apps which we want to run in the Jerry Mouse app server
-export APP_WHITELIST=`cat ./apps_whitelist.txt`
+app_whitelist="$(cat ./apps_whitelist.txt)"
 
 APPSERVER_TARGET=$(pwd)/apps
 
-for dir in ${APP_WHITELIST};
+for dir in ${app_whitelist};
 do
-    MODDIR=${dir%*/}
+    MODDIR="${dir%*/}"
 
     # if the example really is "worth" to be copied, i.e. not empty...
-    if [ -e ../${MODDIR}/mlib/modmain.jar ]
-    then 
-       pushd ../${MODDIR} > /dev/null 2>&1
-    
-       echo "###################################################################################################################################"
-       echo "Copy ${MODDIR}/mlib/*.jar to ${APPSERVER_TARGET}/${MODDIR} ..."
-   
-       mkdir -p ${APPSERVER_TARGET}/${MODDIR}
-       pushd ${APPSERVER_TARGET}/${MODDIR} > /dev/null 2>&1
-          rm -rf *
-          cat > app.json <<EOFAPPJSON
+    if [ ! -e "../${MODDIR}/mlib/modmain.jar" ]; then
+      (cd "../${MODDIR}" && ./all.sh)
+    fi
+
+    pushd "../${MODDIR}" > /dev/null 2>&1
+    echo "###################################################################################################################################"
+    echo "Copy ${MODDIR}/mlib/*.jar to ${APPSERVER_TARGET}/${MODDIR} ..."
+
+    mkdir -p "${APPSERVER_TARGET}"/"${MODDIR}"
+    pushd "${APPSERVER_TARGET}"/"${MODDIR}" > /dev/null 2>&1
+    rm -rf ./*
+
+    cat > app.json <<EOFAPPJSON
 {
   "rootModule": "modmain",
   "bootClass":  "pkgmain.Main",
   "bootMethod": "main"
 }
 EOFAPPJSON
-          mkdir -p ${APPSERVER_TARGET}/${MODDIR}/mlib
-       popd >/dev/null 2>&1 
+    mkdir -p "${APPSERVER_TARGET}"/"${MODDIR}/mlib"
+    popd >/dev/null 2>&1
    
-       pushd mlib > /dev/null 2>&1
-          cp -R *.jar ${APPSERVER_TARGET}/${MODDIR}/mlib
-       popd >/dev/null 2>&1 
+    pushd mlib > /dev/null 2>&1
+    cp -R ./*.jar "${APPSERVER_TARGET}"/"${MODDIR}/mlib"
+    popd >/dev/null 2>&1
        
-       echo " "
-       popd >/dev/null 2>&1 
-    fi
+    echo
+    popd >/dev/null 2>&1
 done
- 
